17172782a074697e5f3e81066853b714f22e9f87
Davien <recd2dmax@gmail.com>
2019-03-02 15:45:55 +0530

    Nav bar added

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className = "Nav">
+      <form>
+        <input
+          type="text"
+          placeholder="Search for Album"
+
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Nav;
+
--- src/dave.js
+++ src/dave.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Album from './Components/Album/Album';
+import Nav from './Components/Nav/Nav';
+import './dave.css';
 
 
-class Dave extends Component{
-  render(){
-    return(
-      <Album />
+class Dave extends Component {
+  render() {
+    return (
+      <div className = "Content_Container">
+        <Nav />
+        <Album />
+      </div>
     );
   }
 }

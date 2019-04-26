3f55da217fdda3ce50488f9f10f7b809158efb82
Davien <recd2dmax@gmail.com>
2019-04-26 15:12:58 +0530

    changed album display and added props to albumhead

--- src/Components/Album/AlbumComponents/AlbumHead.js
+++ src/Components/Album/AlbumComponents/AlbumHead.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const AlbumHead = () => {
+const AlbumHead = (props) => {
   return (
     <div className="AlbumHead">
       <img
         src={"http://localhost:3030/albums/" + props.art + ".jpg"} alt="">
       </img>
       <div className="AlbumDesc">
-        <span><h1>Title: </h1>{props.title}</span><br />
-        <span><h2>Artist: </h2>{props.artist}</span><br />
+        <span><strong>Title: </strong>{props.title}</span><br />
+        <span><strong>Artist: </strong>{props.artist}</span><br />
       </div>
     </div>
   );
